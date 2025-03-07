be1fb7d09764f1f93dd8ba7be461404a74c9edb8
Vimlesh kumari <vimlesh3012@gmail.com>
2025-03-07 11:29:34 +0530

    final team list added

--- react-list-app/src/App.js
+++ react-list-app/src/App.js
@@ -1,37 +1,42 @@
 import { useState } from 'react'
 import './App.css'
-// 
 function App() {
   let [player, setPlayer] = useState("")
   const [playersAdded, setplayersAdded] = useState([])
-
+  const [finalPlayersTeam, setFinalPlayersTeam] = useState([])
 
   const handleChange = (e) => {
     setPlayer(e.target.value)
   }
   const handleSubmit = (e) => {
-    console.log("players", player);
-
-    playersAdded.push(player)
-    setplayersAdded(playersAdded)
-    // setPlayer("")
+    setplayersAdded([...playersAdded, player])
+  }
+  const handleFinalSubmit = (member) => {
+    setFinalPlayersTeam([...finalPlayersTeam, member])
   }
   return (
     <div className="App">
       <input type='text' name="players" onChange={handleChange} value={player} />
-      <button onClick={handleSubmit} disabled={!player}>Add</button>
-      <button onClick={() => setPlayer("")}>Delete</button>
+      <button type="button" onClick={() => handleSubmit()} >Add</button>
+      {/* <button type="button" onClick={() => setPlayer("")}>Delete</button> */}
       <div>
         <h1>Master Team</h1>
-        <div>{playersAdded.length ?
+        <div>{playersAdded.length > 0 ?
           playersAdded.map((ele, ind) => {
-            return <li key={ind}>{ele} <button onClick={handleSubmit} disabled={!player}>Add</button>
+            return <li key={ind}>{ele} <button type='button' onClick={() => handleFinalSubmit(ele)} >Add</button>
               <button onClick={() => setPlayer("")}>Delete</button></li >
-
           })
           : "No Players Added"}</div>
       </div>
+      <div>
+        <h1>Final Team</h1>
+        <div>{finalPlayersTeam.length > 0 ?
+          finalPlayersTeam.map((ele, ind) => {
+            return <li key={ind}>{ele} <button onClick={() => setPlayer("")}>Delete</button></li >
+          })
+          : "No Players Added"}</div>
     </div>
+    </div >
   );
 }
 
